79b4598163f93e4d3b9cbc3a7c0b961732e7cbd4
esseppidefi <97457479+esseppidefi@users.noreply.github.com>
2022-03-11 17:14:56 +0100

    app

---
+++ js/app.js
@@ -0,0 +1,60 @@
+// const items = [
+//     'img/01.jpg',
+//     'img/02.jpg',
+//     'img/03.jpg',
+//     'img/04.jpg',
+//     'img/05.jpg'
+// ];
+
+// const title = [
+//     'Svezia',
+//     'Svizzera',
+//     'Gran Bretagna',
+//     'Germania',
+//     'Paradise'
+// ]
+
+// const text = [
+//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+//     'Lorem ipsum',
+//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+//     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+// ]
+
+const app = new Vue({
+  el: '#root',
+  data: {
+    currentIndex: 0,
+    array: [
+      {
+        titolo: 'Svezia',
+        img: 'img/01.jpg',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+      },
+      {
+        titolo: 'Svizzera',
+        img: 'img/02.jpg',
+        text: 'Lorem ipsum',
+      },
+      {
+        titolo: 'Gran Bretagna',
+        img: 'img/03.jpg',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      },
+      {
+        titolo: 'Germania',
+        img: 'img/04.jpg',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+      },
+      {
+        titolo: 'Paradise',
+        img: 'img/04.jpg',
+        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+      },
+    ],
+  },
+  //   methods: {
+
+  //   }
+});
